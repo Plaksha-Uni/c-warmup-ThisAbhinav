32a8e3422c9e440b36163233aaae289185fb1351
ThisAbhinav <this.abhinav26@gmail.com>
2024-08-12 16:44:07 +0530

    my life is not good

--- pset0/.deps/_cc.d
+++
@@ -1,1 +0,0 @@
-DEP_CC:=cc  -std=gnu2x -Wall -Wextra -Wshadow -g   -fsanitize=undefined -fno-sanitize-recover=undefined -O3
--- pset0/.deps/_cxx.d
+++
@@ -1,1 +0,0 @@
-DEP_CXX:=g++  -std=gnu++2a -Wall -Wextra -Wshadow -g   -fsanitize=undefined -fno-sanitize-recover=undefined -O3 
